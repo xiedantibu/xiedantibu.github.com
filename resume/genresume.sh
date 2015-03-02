9b9b91c8b45f088141a2876c678df4be7c55ee08
xiedantibu <xiedantibu@hotmail.com>
2015-03-02 16:16:29 +0800

    change

--- resume/genresume.sh
+++ resume/genresume.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-title=伍翀的个人简历	#网页标题
-name=伍-翀			#resume.md中的名字中有空格，要用“-”代替
-job=研发工程师		#resume.md中你填的职位
+title=徐灵敏的个人简历	#网页标题
+name=徐-灵-敏			#resume.md中的名字中有空格，要用“-”代替
+job=cocos2d-x游戏开发工程师		#resume.md中你填的职位
 
 pandoc -s -S resume.md -o resume.html -t html5 --self-contained --section-divs --template=resume-template.html -T $title -c css/main.css
 echo 'Generate resume.html success!'
